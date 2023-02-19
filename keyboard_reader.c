59a4639fee6c69a5c3883e5e14f7c1b777281544
Alec Dudley-Bestow <alecdudleybestow@gmail.com>
2023-02-20 10:44:06 +1100

    added key map implementation to keyboard reader

--- keyboard_reader.c
+++ keyboard_reader.c
@@ -1,2 +1,18 @@
 #include "keyboard_reader.h"
+#include "hardware/gpio.h"
 
+void readerInit()   {
+    gpio_init_all();
+    for (size_t i = 0; i < NUM_IN_PINS) {
+        gpio_set_dir(inEnumToNum(i), GPIO_IN);
+    }
+    for (size_t i = 0; i < NUM_OUT_PINS) {
+        gpio_set_dir(outEnumToNum(i), GPIO_OUT);
+    }
+}
+
+bool[NUM_KEYS] getPressedKeys() {
+    for (size_t i = 0; i < NUM_OUT_PINS; i++)  {
+        
+    }
+}
